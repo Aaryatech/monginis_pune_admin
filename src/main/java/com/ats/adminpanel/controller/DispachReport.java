a6fe03250048281142a7058199cb8b954cba5a4f
aaryatech <ASUS@192.168.10.222>
2019-06-05 21:48:50 -0700

    //Changes in station wise dispatch sheet for fast execution  //sumit

--- src/main/java/com/ats/adminpanel/controller/DispachReport.java
+++ src/main/java/com/ats/adminpanel/controller/DispachReport.java
@@ -814,7 +814,7 @@ public class DispachReport {
 			map.add("menuIds", menuIds);
 			System.out.println("map " + map);
 
-			StaionListWithFranchiseeList[] array = restTemplate.postForObject(Constants.url + "/getAbcDepatchReportMin",
+			StaionListWithFranchiseeList[] array = restTemplate.postForObject(Constants.url + "/getAbcDepatchReportMin1",
 					map, StaionListWithFranchiseeList[].class);
 
 			List<StaionListWithFranchiseeList> staionListWithFranchiseeList = new ArrayList<StaionListWithFranchiseeList>(
