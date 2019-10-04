1d5da0c0c4306a04e655a179ea6ebc5feea7e42e
ats-12 <ats-12@ats12>
2019-10-04 17:25:26 +0530

    //Mahesh

--- src/main/java/com/ats/adminpanel/commons/Constants.java
+++ src/main/java/com/ats/adminpanel/commons/Constants.java
@@ -3,7 +3,7 @@ package com.ats.adminpanel.commons;
 
 public class Constants {
 	
-	/*//public static final String url="http://localhost:8097/";
+	//public static final String url="http://localhost:8097/";
 	public static final String url="http://148.72.40.234:8080/bakerywebapi/";
 	public static final String SPCAKE_IMAGE_URL = "http://148.72.40.234:8080/uploadspune/MSPCAKE/";
 	public static final String FR_IMAGE_URL = "http://148.72.40.234:8080/uploadspune/FR/";
@@ -38,8 +38,8 @@ public class Constants {
 	public static int GATE_ENTRY_IMAGE_TYPE=7;
 
 	
-	//public static final String ReportURL ="http://localhost:8978/adminpanel/";
- 	public static final String ReportURL ="http://192.168.1.25:8080/admin/";//change
+	//public static final String ReportURL ="http://localhost:9376/adminpanel/";
+	public static final String ReportURL ="http://192.168.1.25:8080/admin/";//change
 
 	public static final String SETTING_KEY = "PB";
 
@@ -54,76 +54,22 @@ public class Constants {
 	public static final int AP_BY_GATE = 2;
 
 	public static final int DIS_BY_GATE = 3;
-	 public static final String REPORT_SAVE = "/home/supertom/apache-tomcat-8.5.35/webapps/admin/Report.pdf";
-	//	public static final String REPORT_SAVE = "/home/ats-12/Report.pdf";
+    public static final String REPORT_SAVE = "/home/supertom/apache-tomcat-8.5.35/webapps/admin/Report.pdf";
+	
+	public static final String BILL_REPORT_PATH="/home/supertom/apache-tomcat-8.5.35/webapps/admin/bill.pdf";
+    public static final String FINISHEDGOOD_REPORT_PATH="/home/supertom/apache-tomcat-8.5.35/webapps/admin/fsr.pdf";
+	public static final String CRN_REPORT_PATH="/home/supertom/apache-tomcat-8.5.35/webapps/admin/crn.pdf";
+	public static final String PO_PDF_PATH="/home/supertom/apache-tomcat-8.5.35/webapps/admin/po.pdf";
+    public static final String SALES_REPORT_PATH="/home/supertom/apache-tomcat-8.5.35/webapps/admin/report.pdf";
+    public static final String INWARD_PATH="/home/supertom/apache-tomcat-8.5.35/webapps/admin/Inward.pdf";
+    public static final String LOG_PDF_PATH="/home/supertom/apache-tomcat-8.5.35/webapps/admin/logdf.pdf";
+    public static final String DISPATCH_PATH="/home/supertom/apache-tomcat-8.5.35/webapps/admin/dispatch.pdf";
 
 	public static final String SP_CAKE_FOLDER = "http://148.72.40.234:8080/uploadspune/SPCAKE/";
 
 	public static final String CUST_CHOICE_PHOTO_CAKE_FOLDER = "http://148.72.40.234:8080/uploadspune/CUSTCHOICEPHOTOCAKE/";
 	public static final int LOGIS_BILL_PDF_TYPE = 8;
 	public static final String LOGIS_BILL_URL = "http://148.72.40.234:8080/uploadspune/MSPCAKE/";
-	public static final int MENU_IMAGE_TYPE = 0;
-	public static final String MENU_IMAGE_URL = null;*/
-	
-	//public static final String url="http://localhost:8097/";
-	public static final String url="http://97.74.228.55:8080/webapi/";
-	public static final String SPCAKE_IMAGE_URL = "http://97.74.228.55:8080/uploads/MSPCAKE/";
-	public static final String FR_IMAGE_URL = "http://97.74.228.55:8080/uploads/FR/";
-	public static final String MESSAGE_IMAGE_URL = "http://97.74.228.55:8080/uploads/MSG/";
-	public static final String ITEM_IMAGE_URL = "http://97.74.228.55:8080/uploads/ITEM/";
-	public static final String RAW_MAT_IMG_URL ="http://97.74.228.55:8080/uploads/RAWMAT/";
-	public static final String GATE_ENTRY_IMG_URL = "http://97.74.228.55:8080/uploads/GATEENTRY/";
-	public static final String GVN_IMAGE_URL ="http://97.74.228.55:8080/uploads/GVN/";
-	public static  String FACTORYNAME = "Siddharth Foods";
-	public static  String FACTORYADDRESS = "C-2, A/8, Plot No. 183/1, Phase - 1 ,GIDC Naroda, Ahmedabad - 382330";
-	public static final String FACTORYGSTIN = "-";
-	public static final String CITY = "Ahmedabad";
-	public static final String STATE = "GUJARAT";
-	public static final String CODE = "SF";
-
-	// navigation view
-	
-	public static int mainAct=0;
-	public static int subAct=0;
-
-	public static int FR_IMAGE_TYPE=1;
-	public static int ITEM_IMAGE_TYPE=2;
-	public static int MESSAGE_IMAGE_TYPE=3;
-	
-	public static int SPCAKE_IMAGE_TYPE=4;
-	
-	public static int CUST_CHIOICE_IMAGE_TYPE=5;
-	
-	
-	public static int RAW_MAT_IMAGE_TYPE=6;
-
-	public static int GATE_ENTRY_IMAGE_TYPE=7;
-
-	
-	//public static final String ReportURL ="http://localhost:8978/adminpanel/";
- 	public static final String ReportURL ="http://97.74.228.55:8080/admin/";//change
-
-	public static final String SETTING_KEY = "PB";
-
-	public static final int DIS_BY_ACC = 7;
-
-	public static final int AP_BY_ACC = 6;
-
-	public static final int DIS_BY_STORE = 5;
-
-	public static final int AP_BY_STORE = 4;
-
-	public static final int AP_BY_GATE = 2;
-
-	public static final int DIS_BY_GATE = 3;
-	 public static final String REPORT_SAVE = "/opt/apache-tomcat-8.5.37/webapps/uploads/AdminReport.pdf";
-	//	public static final String REPORT_SAVE = "/home/ats-12/Report.pdf";
-
-	public static final String SP_CAKE_FOLDER = "http://97.74.228.55:8080/uploads/SPCAKE/";
-
-	public static final String CUST_CHOICE_PHOTO_CAKE_FOLDER = "http://97.74.228.55:8080/uploads/CUSTCHOICEPHOTOCAKE/";
-	public static final int LOGIS_BILL_PDF_TYPE = 8;
-	public static final String LOGIS_BILL_URL = "http://97.74.228.55:8080/uploads/MSPCAKE/";
 	public static final int MENU_IMAGE_TYPE = 0;
 	public static final String MENU_IMAGE_URL = null;
 }
--- src/main/java/com/ats/adminpanel/commons/VpsImageUpload.java
+++ src/main/java/com/ats/adminpanel/commons/VpsImageUpload.java
@@ -14,7 +14,7 @@ public class VpsImageUpload {
 	
 
 	
-	/*	public static final String FR_FOLDER = "/home/supertom/apache-tomcat-8.5.35/webapps/uploads/FR/";
+	public static final String FR_FOLDER = "/home/supertom/apache-tomcat-8.5.35/webapps/uploads/FR/";
 	public static final String ITEM_FOLDER = "/home/supertom/apache-tomcat-8.5.35/webapps/uploads/ITEM/";
 
 	public static final String MSG_FOLDER = "/home/supertom/apache-tomcat-8.5.35/webapps/uploads/MSG/";
@@ -26,20 +26,9 @@ public class VpsImageUpload {
 	public static final String GATE_ENTRY_IMAGE_FOLDER = "/home/supertom/apache-tomcat-8.5.35/webapps/uploads/GATEENTRY/";
 	
 	public static final String LOGIS_BILL_FILE= "/home/supertom/apache-tomcat-8.5.35/webapps/uploads/MSPCAKE/";
-*/
-	
-	public static final String FR_FOLDER = "/opt/apache-tomcat-8.5.37/webapps/uploads/FR/";
-	public static final String ITEM_FOLDER = "/opt/apache-tomcat-8.5.37/webapps/uploads/ITEM/";
 
-	public static final String MSG_FOLDER = "/opt/apache-tomcat-8.5.37/webapps/uploads/MSG/";
 	
-	public static final String M_SP_CAKE_FOLDER = "/opt/apache-tomcat-8.5.37/webapps/uploads/MSPCAKE/";
-	
-	public static final String RAW_MAT_IMAGE_FOLDER = "/opt/apache-tomcat-8.5.37/webapps/uploads/RAWMAT/";
 
-	public static final String GATE_ENTRY_IMAGE_FOLDER = "/opt/apache-tomcat-8.5.37/webapps/uploads/GATEENTRY/";
-	
-	public static final String LOGIS_BILL_FILE= "/opt/apache-tomcat-8.5.37/webapps/uploads/MSPCAKE/";
 
 	private static final String FIELDMAP_FOLDER = null;
 	private static final String KYC_FOLDER = null;
